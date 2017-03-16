42e73aecf2650a67c76ab2a5acb304da21010a75
Daniel Barlow <dan@telent.net>
2017-03-16 13:47:17 +0000

    first commit

---
+++ default.nix
@@ -0,0 +1,24 @@
+with import <nixpkgs> {}; 
+let
+  version = "0.1";
+  nodePackages = import ./node-default.nix {
+    inherit pkgs system;
+  };
+  clojurescriptJs = fetchurl {
+    url = "https://gist.githubusercontent.com/kanaka/b588dbb6d44174672cc2/raw/repl-node.js";
+    sha256 = "0msz5ikb7k8akww7q4q2m2b85mvpxcbsirad2132jdrswlpsap2n";
+  };
+in
+  stdenv.mkDerivation rec {
+    name = "paxton-${version}";
+    src = ./. ;
+    nativeBuildInputs = [ makeWrapper ];
+    buildInputs = [ nodePackages.package wayland wlc libxkbcommon pixman
+     fontconfig pcre json_c pango cairo libinput libcap nodejs dbus_libs
+      ];
+    repl = "${nodejs}/bin/node ${clojurescriptJs}";
+    LD_LIBRARY_PATH = stdenv.lib.makeLibraryPath [ wlc dbus_libs ];
+    NODE_PATH = "${nodePackages.package}/lib/node_modules/paxton/node_modules" ;
+
+  }
+
